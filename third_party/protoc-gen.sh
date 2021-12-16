0c5025ce36f4e95c26fc994d7186cdb3b90a9bbe
Jonah_Banda <jonah.banda@fao.org>
2021-12-16 23:37:27 +0200

    second commit

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,1 +1,1 @@
-protoc --go_out=./pkg  --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./api/proto/v1/todo-service.proto
+protoc --go_out=./pkg  --go_opt=paths=source_relative --go-grpc_out=./pkg  --go-grpc_opt=paths=source_relative ./api/proto/v1/todo-service.proto
